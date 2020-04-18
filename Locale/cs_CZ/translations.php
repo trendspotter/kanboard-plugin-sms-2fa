80942495e91af744b23d717c05496d40b2f78c2c
trendspotter <j.podhorecky@volny.cz>
2020-04-18 18:04:11 +0200

    Add cs_CZ translations

---
+++ Locale/cs_CZ/translations.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'SMS - Text Message' => 'SMS - Textová zpráva',
+    'Use SMS messages for two-factor authentication' => 'Použít SMS zprávy pro dvoufaktorové ověření',
+    'Kanboard code: %d' => 'Kód Kanboard: %d',
+    'SMS Two-Factor Authentication' => 'SMS dvoufaktorové ověření',
+    'Phone Number' => 'Telefonní číslo',
+    'Twilio Account SID' => 'Twilio Account SID',
+    'Twilio Auth Token' => 'Twilio Auth Token',
+    'Twilio Phone Number' => 'Twilio telefonní číslo',
+);
